8f9bcaed1f9571bf0aef736edcc33ad2973c5608
Michalis Pardalos <mpardalos@gmail.com>
2016-10-17 12:51:00 +0300

    move Subject class to models module

--- main/solver.py
+++ main/solver.py
@@ -7,21 +7,7 @@ import json
 import itertools
 from ortools.constraint_solver import pywrapcp
 
-
-class Subject:
-    def __init__(self, name: str, periods_per_week: int) -> None:
-        self.name = name
-        self.periods_per_week = periods_per_week
-        self.period_names = ['{}-p{}'.format(name, i) for i in range(1, periods_per_week+1)]
-        
-    def __eq__(self, other):
-        return isinstance(other, type(self)) and self.name == other.name
-
-    def __hash__(self) -> int:
-        return hash(self.name)
-
-    def __repr__(self) -> str:
-        return "Subject: {}".format(self.name)
+from models import Subject
 
 
 def possible_timetables(students: Sequence[Sequence[Subject]], periods_per_week: int) -> Generator[Dict[str, int], None, None]:
