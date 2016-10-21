d31f773719f3554a380865d4bdbf414882711bdb
Michalis Pardalos <mpardalos@gmail.com>
2016-10-21 23:12:56 +0300

    Switch to QtWidgets from QtQuick
    Instead of using qml and QtQuick, we will be using QtDesigner and importing the ui
    files directly into python using PyQt5.uic

--- main/interface.py
+++ main/interface.py
@@ -1,16 +1,24 @@
-import sys 
+import os
+import sys
 
+from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtCore import QUrl
-from PyQt5.QtQuick import QQuickView
-       
-    
-if __name__ == "__main__":
-    
-    app = QApplication(sys.argv)
-  
-    view = QQuickView()
-    view.setSource(QUrl('ui/subject_list.qml'))
-    view.show()
 
+MainWindowUI = uic.loadUiType(os.path.join('ui', 'main.ui'))[0]
+
+
+class MainWindow(QMainWindow, MainWindowUI):
+    def __init__(self, parent=None):
+        # noinspection PyArgumentList
+        QMainWindow.__init__(self, parent)
+        self.setupUi(self)
+
+        # bind handlers
+        self.actionQuit.triggered.connect(self.close)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
     sys.exit(app.exec_())
