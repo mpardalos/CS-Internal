3557791083cb90bca344c486a54fd055c7eddbf5
Michalis Pardalos <mpardalos@gmail.com>
2016-12-03 20:16:05 +0200

    Add Subject loader for xlsx files

--- main/models.py
+++ main/models.py
@@ -1,17 +1,72 @@
-import itertools
-import json
-from collections import defaultdict
-from typing import List, TextIO
+from typing import List, Generator, Sequence
+
+import sys
+from openpyxl import load_workbook
+from openpyxl.worksheet import Worksheet
+from openpyxl.cell import Cell
+
+# Dirty dirty (but awesome) hack for debugging
+# e.g. the cell B4 containing 'test' would be repr'd as 'B4: test'
+# noinspection PyUnresolvedReferences
+Cell.__repr__ = lambda self: "{coords}: {value}".format(coords=self.coordinate, value=self.value)
+
+def _find_in_cells(cells: Sequence[Cell], text: str) -> int:
+    """
+    Find the cell containing :param text: in :param cells:
+    :return: the index of the cell found in :param cells: or -1 if it is not found
+    """
+    try:
+        return next(col_cell_idx for col_cell_idx, col_cell in enumerate(cells) if
+                    str(col_cell.value or '').lower().strip() == text.lower().strip())
+    except StopIteration:
+        return -1
+
+class Datastore:
+    def __init__(self, fp: str, worksheet_name='Classes'):
+        self.worksheet = load_workbook(fp)[worksheet_name] # type: Worksheet
+
+        # TODO: Add validation
+
+    def get_subjects(self) -> Generator['Subject', None, None]:
+        subject_name_row = list(self.worksheet.rows)[0][1:] # type: List[Cell]
+        columns = list(self.worksheet.columns)
+
+        for cell in subject_name_row:
+            column = columns[cell.col_idx-1]
+
+            name = column[0].value
+            if name == '': break
+
+            teacher_name = column[1].value
+
+            sl_periods_per_week = column[2].value
+
+            hl_periods_per_week = column[3].value
+
+            # find index of the cell containing the text 'hl' and 'sl'
+            sl_marker_index = _find_in_cells(column, 'sl')
+            hl_marker_index = _find_in_cells(column, 'hl')
+            ending_marker_index = _find_in_cells(column, '') # TODO: handle early empty cell
+            sl_students = [cell.value for cell in column[sl_marker_index+1:hl_marker_index]]
+            hl_students = [cell.value for cell in column[hl_marker_index+1:ending_marker_index]]
+
+            # SL
+            yield Subject(name + '-sl', sl_periods_per_week, teacher_name, sl_students)
+            # HL
+            yield Subject(name + '-hl', hl_periods_per_week, teacher_name, hl_students)
+
+
 
 
 class Subject:
-    def __init__(self, name: str, periods_per_week: int, student_names: List[str] = None) -> None:
+    def __init__(self, name: str, periods_per_week: int, teacher_name: str,  student_names: List[str] = None) -> None:
         self.name = name
         self.periods_per_week = periods_per_week
-        self.period_names = ['{}-p{}'.format(name, i) 
+        self.period_names = ['{}-p{}'.format(name, i)
                 for i in range(1, periods_per_week+1)]
         self.student_names = student_names or []
-        
+        self.teacher_name = teacher_name
+
     def __eq__(self, other):
         return isinstance(other, type(self)) and self.name == other.name
 
@@ -21,35 +76,8 @@ class Subject:
     def __repr__(self) -> str:
         return "Subject: {}".format(self.name)
 
-    @staticmethod
-    def subjects_from_json_store(fp: TextIO) -> List:
-        data = json.load(fp)
-        subjects = [Subject(name, 3 if 'HL' in name else 2, students) for name, students in data.items()]
-        return subjects
 
-
-def students_from_json_store(fp: TextIO) -> List:
-    """
-    Create a list of students and their subjects from a json file containing an object of the form:
-    {
-        subject_name: [student1, student2, ...]
-    }
-
-    Args:
-        fp: the file from which to load the data
-    """
-    # The json input is subjects mapped to their students
-    subjects = json.load(fp)
-
-    # Invert the json input, from map of subjects -> student to one of students -> subjects
-    student_names = set(itertools.chain(*subjects.values()))
-
-    students = defaultdict(list)  # type: defaultdict[str, List[Subject]]
-    for subject_name, subject_students in subjects.items():
-        for student_name in student_names:
-            if student_name in subject_students:
-                students[student_name].append(Subject(subject_name, 3 if 'HL' in subject_name else 2))
-
-    return list(students.values())
-
-
+if __name__ == '__main__':
+    for s in Datastore(sys.argv[1]).get_subjects():
+        print(s)
+        print([s.teacher_name] + s.student_names)
