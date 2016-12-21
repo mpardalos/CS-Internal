498727da1166b5f4dc26e7ca6e88b6ed04769153
Michalis Pardalos <mpardalos@gmail.com>
2016-12-21 14:07:04 +0200

    Get both sl and hl students from datastore if they exist

--- main/models.py
+++ main/models.py
@@ -1,9 +1,7 @@
 import itertools
 import sys
 from collections import defaultdict
-from pprint import pprint
 from typing import List, Sequence, Dict, Iterator
-from typing import Tuple
 
 from openpyxl import load_workbook
 from openpyxl.cell import Cell
@@ -29,7 +27,7 @@ def _find_in_cells(cells: Sequence[Cell], text: str) -> int:
 class LoadingError(Exception):
     """
     Raised for problems in loading the xlsx file containing the students and subjects
-    :argument problem_cell: the cell which caused the r
+    :argument problem_cell: the cell which caused the problem
     """
     def __init__(self, msg, cell: Cell=None):
         super().__init__(msg)
@@ -37,19 +35,17 @@ class LoadingError(Exception):
 
 
 class Datastore:
+    student_list_start_row_index = 4
     def __init__(self, fp: str, worksheet_name='Classes'):
         self.worksheet = load_workbook(fp)[worksheet_name]  # type: Worksheet
 
-        # TODO: check that both SL and HL tags are present
         last_column_index = next(idx for idx, col in enumerate(self.worksheet.columns) if col[0].value in (None, ''))
 
-        for column in itertools.islice(self.worksheet.columns, 0, last_column_index):
-            if '' in [cell.value or '' for cell in column[0:5]]:
-                raise LoadingError('All data must be provided for each subject',
-                                   next(cell for cell in column if cell.value in (None, ''))
-                                   )
 
     def get_subjects(self) -> Iterator['Subject']:
+        """
+        :return: All subject objects contained in the datastore
+        """
         subject_name_row = list(self.worksheet.rows)[0][1:]  # type: List[Cell]
         columns = list(self.worksheet.columns)
 
@@ -57,26 +53,27 @@ class Datastore:
             column = columns[cell.col_idx - 1]
 
             name = column[0].value
-            if name == '' or name is None:
+            if name in ('', None):
                 break
 
             teacher_name = column[1].value
-
             sl_periods_per_week = column[2].value
+            extra_hl_periods_per_week = column[3].value or 0
+            hl_marker_index = _find_in_cells(column, 'HL')
+            ending_marker_index = _find_in_cells(column[4:], '') + 4 # Index of the first empty cell after the students
+            if ending_marker_index <= 4: # occurs if the empty cell is not in the cells returned by the library
+                ending_marker_index = len(column)
 
-            hl_periods_per_week = column[3].value
+            # If there is only one group in the subject
+            if hl_marker_index == -1:
+                students = [cell.value for cell in column[4:ending_marker_index]]
+                yield Subject(name, sl_periods_per_week, teacher_name, students)
+            # If there are both sl and hl students in the subject
+            else:
+                sl_students = [cell.value for cell in column[4:hl_marker_index]]
+                hl_students = [cell.value for cell in column[hl_marker_index+1:ending_marker_index]]
+                yield Subject(name, sl_periods_per_week + extra_hl_periods_per_week, teacher_name, sl_students, hl_students)
 
-            # find index of the cell containing the text 'hl' and 'sl'
-            sl_marker_index = _find_in_cells(column, 'sl')
-            hl_marker_index = _find_in_cells(column, 'hl')
-            ending_marker_index = _find_in_cells(column, '')  # TODO: handle early empty cell
-            sl_students = [cell.value for cell in column[sl_marker_index + 1:hl_marker_index]]
-            hl_students = [cell.value for cell in column[hl_marker_index + 1:ending_marker_index]]
-
-            # SL
-            yield Subject(name + ' SL', sl_periods_per_week, teacher_name, sl_students)
-            # HL
-            yield Subject(name + ' HL', hl_periods_per_week, teacher_name, hl_students)
 
     def get_students(self) -> Dict[str, List['Subject']]:
         subjects = self.get_subjects()
@@ -94,12 +91,13 @@ class Datastore:
 
 
 class Subject:
-    def __init__(self, name: str, periods_per_week: int, teacher_name: str, student_names: List[str] = None) -> None:
+    def __init__(self, name: str, periods_per_week: int, teacher_name: str, student_names: List[str], hl_student_names: List[str] = None) -> None:
         self.name = name
         self.periods_per_week = periods_per_week
         self.period_names = ['{}-p{}'.format(name, i)
                              for i in range(1, periods_per_week + 1)]
-        self.student_names = student_names or []
+        self.student_names = student_names
+        self.hl_student_names = hl_student_names or []
         self.teacher_name = teacher_name
 
     def __eq__(self, other) -> bool:
@@ -116,7 +114,7 @@ def main():
     datastore = Datastore(sys.argv[1])
     for s in datastore.get_subjects():
         print(s)
-        print([s.teacher_name] + s.student_names)
+        print([s.teacher_name] + ['sl: '] + list(s.student_names) + ['hl: '] + list(s.hl_student_names))
 
     pprint(datastore.get_students())
 
